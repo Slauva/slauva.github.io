d5fd798a5dca82396064da63d31e82b026ba0563
Slauva <main-app@yandex.ru>
2019-06-25 17:35:35 +0500

    new

--- MoGo/assets/js/app.js
+++ MoGo/assets/js/app.js
@@ -14,10 +14,31 @@ $(function() {
         // console.log(scrollOffset);
         scrollOffset >= introH ? header.addClass('fixed') : header.removeClass('fixed');
     }
+});
 
-    // Smooth scroll
-    $('[data-scroll]').on('click', function(event) {
-        event.preventDefault();
-        var blockId = $(this).data('scroll');
-    });
+// Smooth scroll
+$('[data-scroll]').on('click', function(event) {
+    event.preventDefault();
+    var $this = $(this),
+        blockId = $this.data('scroll'),
+        blockOffset = $(blockId).offset().top;
+    $('.nav a').removeClass('active');
+    $this.addClass('active');
+    $('html, body').animate(
+        {
+            scrollTop: blockOffset
+        },
+        500
+    );
+});
+
+// Nav toggle
+
+$('.nav-toggle').click(function(e) {
+    e.preventDefault();
+
+    var $this = $(this),
+        nav = $('.nav');
+    nav.toggleClass('active');
+    $this.toggleClass('active');
 });
